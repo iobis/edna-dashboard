dddefacf5ce67813bc054ea17fabd034c45bca15
Pieter Provoost <pieterprovoost@gmail.com>
2024-07-23 14:12:24 +0200

    generate image list

--- R/images.R
+++ R/images.R
@@ -1,2 +1,26 @@
+library(rgbif)
+library(purrr)
+library(glue)
+
 #' Generate list of GBIF image URLs, one for every species in the dataset
-generate_image_list <- function() {}
+generate_image_list <- function() {
+  occurrence <- read_occurrence_data()
+  
+  species_names <- occurrence %>% 
+    filter(taxonRank == "species") %>% 
+    pull(scientificName) %>% 
+    unique()
+  
+  gbif_images <- map(species_names, function(species_name) {
+    message(glue("Fetching GBIF image for species name {species_name}"))
+    url <- occ_search(scientificName = species_name, mediaType = "StillImage", limit = 1, curlopts = list(timeout_ms = 10000))$media[[1]][[1]][[1]]$identifier
+    url
+  }) %>% 
+    map(~ifelse(is.null(.x), NA, .x)) %>% 
+    unlist() 
+
+  data.frame(species = species_names, image_url = gbif_images)
+}
+
+df <- generate_image_list()
+write.table(df, "data/images.txt", row.names = FALSE, quote = FALSE, sep = "\t", na = "")
