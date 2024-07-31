81431ef5953cce6bd0b7846564930909ebbebbc9
SSuominen1 <s.suominen@unesco.org>
2024-07-31 18:24:18 +0200

    point to data files in output folder

--- R/occurrence.R
+++ R/occurrence.R
@@ -4,7 +4,7 @@ library(purrr)
 
 #' Reads the full occurrence dataset.
 read_occurrence_data <- function() {
-  occurrence_files <- list.files("output", "*Occurrence*", full.names = TRUE)
+  occurrence_files <- list.files("data/output", "*Occurrence*", full.names = TRUE)
   occurrence <- map(occurrence_files, read.table, sep = "\t", quote = "", header = TRUE, comment.char = "") %>%
     bind_rows() %>%
     mutate_if(is.character, na_if, "")
