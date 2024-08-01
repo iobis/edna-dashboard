e1f0bdb8cecef1d0c57ded71c321bfec7d2841ec
SSuominen1 <s.suominen@unesco.org>
2024-08-01 11:19:16 +0200

    one function for value boxes

--- R/value_boxes.R
+++ R/value_boxes.R
@@ -1,55 +1,41 @@
 #Calculate base values based on site selection
 #Calculate all 4 values in a list
 
+
+
 n_species <- function(site, occurrence){
+    
+    data <- list()
 
-    occurrence %>% 
+    #site="Lagoons of New Caledonia: Reef Diversity and Associated Ecosystems"
+
+    occurrence_species <- occurrence %>% 
     filter(higherGeography==site) %>% 
-    filter(taxonRank=="species") %>%   
-    summarise(unique_species = n_distinct(scientificName)) %>%
-    pull(unique_species)
-
-}
-
-
-n_fish <- function(site, occurrence){
+    filter(taxonRank=="species")
 
     groups=read.csv("data/supporting_data/groups.csv")
-
-    occurrence %>%
-    filter(higherGeography==site) %>% 
-    filter(taxonRank=="species") %>%
-    filter(class %in% groups[groups$group=="fish", "taxon"]) %>%
-    summarise(unique_species = n_distinct(scientificName)) %>%
-    pull(unique_species)
-
-}
-
-n_mammal <- function(site, occurrence){
-
-    groups=read.csv("data/supporting_data/groups.csv")
-
-    occurrence %>%
-    filter(higherGeography == site) %>% 
-    filter(taxonRank == "species") %>%
-    filter(class %in% groups[groups$group=="mammals", "taxon"]) %>%
-    summarise(unique_species = n_distinct(scientificName)) %>%
-    pull(unique_species)
-
-}
-
-n_iucn<- function(site, occurrence){
-
     redlist <- read.csv("data/supporting_data/redlist.csv")
-    redlist_cat <- redlist %>% 
-                        filter(category %in% c("EN", "CR", "VU")) 
+    redlist_cat <- redlist %>% filter(category %in% c("EN", "CR", "VU")) 
 
-    occurrence %>%
-    filter(higherGeography==site) %>% 
-    filter(taxonRank=="species") %>%
-    filter(scientificName %in% redlist_cat$species) %>%
-    summarise(unique_species = n_distinct(scientificName)) %>%
-    pull(unique_species)
+    data$n_species <- occurrence_species %>%   
+        summarise(unique_species = n_distinct(scientificName)) %>%
+        pull(unique_species)
 
+    data$n_fish <- occurrence_species %>%  
+        filter(class %in% groups[groups$group=="fish", "taxon"]) %>%
+        summarise(unique_species = n_distinct(scientificName)) %>% 
+        pull(unique_species)
+    
+    data$n_mammals <- occurrence_species %>%  
+        filter(class %in% groups[groups$group=="mammals", "taxon"]) %>%
+        summarise(unique_species = n_distinct(scientificName)) %>% 
+        pull(unique_species)
+    
+    data$n_iucn <- occurrence_species %>%  
+        filter(scientificName %in% redlist_cat$species) %>%
+        summarise(unique_species = n_distinct(scientificName)) %>% 
+        pull(unique_species)
+
+    return(data)
 }
 
