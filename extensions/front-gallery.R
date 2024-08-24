643c12e723dfa90bf08181b40497acdf604ceb6f
silasprincipe <53846571+silasprincipe@users.noreply.github.com>
2024-08-24 10:26:19 +0200

    Add control for Krona plots and front gallery

--- extensions/front-gallery.R
+++ extensions/front-gallery.R
@@ -1,5 +1,30 @@
-front_gallery <- function(site, images_table) {
-  images_table <- data.table::fread(images_table)
+#' A simple image gallery
+#'
+#' @param site name of the site
+#' @param images_table a data.frame or a character with a path to a table
+#'   containing three columns: site (site name), image_url (path to image),
+#'   and caption. If the last is missing for any image, then the site name
+#'   is used as a caption
+#'
+#' @return html component
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#'  images_table <- data.frame(
+#' image_url = c("https://s1.static.brasilescola.uol.com.br/be/2020/06/oceano.jpg",
+#'               "https://static.wixstatic.com/media/c4e423_2d038a02a77240ab8999aa853e85c01e~mv2.jpg/v1/fill/w_640,h_460,al_c,q_80,usm_0.66_1.00_0.01,enc_auto/c4e423_2d038a02a77240ab8999aa853e85c01e~mv2.jpg"),
+#' caption = c("test 1", "test 2"),
+#' site = "site"
+#' )
+#' front_gallery("site", images_table)
+#' }
+front_gallery <- function(site, images_table, show_counter = TRUE) {
+  
+  if (is.character(images_table)) {
+    images_table <- data.table::fread(images_table)
+  }
+  
   images_table_sel <- images_table[images_table$site == site, ]
   total_length <- nrow(images_table_sel)
   
@@ -27,6 +52,16 @@ front_gallery <- function(site, images_table) {
       ")"
     )))
     
+    if (show_counter) {
+      counter_obj <- htmltools::tags$div(
+        id = "carousel-counter",
+        class = "carousel-counter",
+        paste0("1/", total_length) # Initially show "1/3" assuming you have 3 images
+      )
+    } else {
+      counter_obj <- NULL
+    }
+    
     # Amazingly done by ChatGPT:
     # Create the image gallery HTML structure
     output <- htmltools::div(
@@ -35,11 +70,7 @@ front_gallery <- function(site, images_table) {
       `data-bs-ride` = "carousel",
       
       # Counter for current image and total images
-      htmltools::tags$div(
-        id = "carousel-counter",
-        class = "carousel-counter",
-        "1/3" # Initially show "1/3" assuming you have 3 images
-      ),
+      counter_obj,
       
       # Carousel inner
       inner_obj,
