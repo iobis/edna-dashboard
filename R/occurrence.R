0e044918d1ea7f748e4c629547b5f48acdaecce7
SSuominen1 <s.suominen@unesco.org>
2024-08-28 15:42:20 +0200

    add qc steps to reading occurrence data

--- R/occurrence.R
+++ R/occurrence.R
@@ -23,17 +23,74 @@ read_occurrence_data <- function() {
  #Fix taxonomic levels 
  worms_tax_levels <- read.csv2("data/supporting_data/worms_tax_levels.csv")
 
-#  merge the taxonomic information to the dataframe by aphiaID
+#  merge the taxonomic information to the dataframe by aphiaID only if the value is not NA
+#occurrence <- occurrence %>%
+#  left_join(worms_tax_levels, by = "aphiaID") %>%
+#  mutate(
+#    kingdom = coalesce(kingdom.y, kingdom.x),
+#    phylum = coalesce(phylum.y, phylum.x),
+#    class = coalesce(class.y, class.x),
+ #   order = coalesce(order.y, order.x),
+#   family = coalesce(family.y, family.x),
+#   genus = coalesce(genus.y, genus.x)
+ # ) %>%
+ # select(-ends_with(".x"), -ends_with(".y"))  # Clean up the columns
+
+# replace all values based on the worms values (also na) 
 occurrence <- occurrence %>%
   left_join(worms_tax_levels, by = "aphiaID") %>%
   mutate(
-    kingdom = coalesce(kingdom.y, kingdom.x),
-    phylum = coalesce(phylum.y, phylum.x),
-    class = coalesce(class.y, class.x),
-    order = coalesce(order.y, order.x),
-    family = coalesce(family.y, family.x),
-    genus = coalesce(genus.y, genus.x)
+    kingdom = kingdom.y,
+    phylum = phylum.y,
+    class = class.y,
+    order = order.y,
+    family = family.y,
+    genus = genus.y
   ) %>%
-  select(-ends_with(".x"), -ends_with(".y"))  # Clean up the columns
+  select(-ends_with(".x"), -ends_with(".y"))
+
+#Other qc (still remaining, add this to a previous workflow step)
+
+#Remove primates (already added to qc, but remove for now here)
+occurrence = occurrence %>% filter(order!="Primates"|is.na(order))
+
+
+#Issues with blast results for the 12Steleo fragment. Consider removing these at least at species level (or filtering from blast results)
+#occurrence %>% filter(pcr_primer_name_forward=="teleo_F_L1848",  grepl("blast", identificationRemarks, ignore.case = TRUE), taxonRank=="species")
+
+#-------------------------#
+# Remove singletons - to be discussed?
+#-------------------------#
+
+occurrence_clean_no_singletons <- occurrence  %>%
+  group_by(DNA_sequence, higherGeography) %>%
+  mutate(total_abundance = sum(organismQuantity)) %>%
+  ungroup() %>%
+  filter(total_abundance > 1) %>%
+  select(-total_abundance)
+
+occurrence <- occurrence_clean_no_singletons
+
+#-------------------------#
+# Remove AA's and CC's
+#-------------------------#
+is_all_A_or_C <- function(seq) {
+  return(grepl("^[AC]+$", seq))
+}
+
+occurrences <- occurrence %>%
+  filter(!sapply(DNA_sequence, is_all_A_or_C))
+
+#-------------------------#
+# Remove prokaryotic contaminants after renaming
+#-------------------------#
+ occurrence <- occurrence %>% filter(kingdom !="Bacteria"|is.na(kingdom), kingdom !="Archaea"|is.na(kingdom), kingdom !="Fungi"|is.na(kingdom), kingdom !="Viruses"|is.na(kingdom))
+
+#-------------------------#
+# Remove cross-contaminants?
+#-------------------------#
+
+#These mostly removed in the annotation step. 
+
 
 }
