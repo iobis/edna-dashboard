80f956ca882acc66bb722435defceb3e15f1266b
Pieter Provoost <pieterprovoost@gmail.com>
2024-09-05 11:40:37 +0200

    cache occurrence as parquet

--- R/occurrence.R
+++ R/occurrence.R
@@ -1,9 +1,10 @@
 library(dplyr)
 library(stringr)
 library(purrr)
+library(arrow)
 
-#' Reads the full occurrence dataset.
-read_occurrence_data <- function() {
+#' Reads the full occurrence dataset from TSV.
+read_occurrence_tsv <- function() {
   occurrence_files <- list.files("data/output", "*Occurrence*", full.names = TRUE)
   
   dna_files <- list.files("data/output", "*DNADerivedData*", full.names = TRUE)
@@ -17,5 +18,23 @@ read_occurrence_data <- function() {
     mutate_if(is.character, na_if, "")%>%
     left_join(dna, by = "occurrenceID")
 
+  return(occurrence)
+}
+
+#' Converts the occurrence dataset to parquet.
+convert_occurrence_data <- function() {
+  message("Converting TSV to parquet...")
+  occurrence <- read_occurrence_tsv()
+  write_parquet(occurrence, "data/output/occurrence.parquet")
+  return(occurrence)
+}
+
+#' Reads the full occurrence dataset from parquet, converts TSV to parquet if the parquet file does not exist.
+read_occurrence_data <- function() {
+  if (!file.exists("data/output/occurrence.parquet")) {
+    occurrence <- convert_occurrence_data()  
+  } else {
+    occurrence <- read_parquet("data/output/occurrence.parquet")
+  }
   return(occurrence)
 }
