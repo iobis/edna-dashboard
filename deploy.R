f50aec471a222941dae2caa1efa2082301ca989a
silasprincipe <53846571+silasprincipe@users.noreply.github.com>
2024-10-20 16:27:35 +0200

    Update deploy.R

--- deploy.R
+++ deploy.R
@@ -1,13 +1,24 @@
 # Deploy the dashboard on the server
+force <- FALSE
 
 # Download occurrence data
-cat("Downloading occurrence data ======\n")
-system("rm -r output output.zip")
-download.file("https://obis-edna-results.s3.amazonaws.com/output.zip",
-              "output.zip")
-unzip("output.zip", exdir = "data/")
-file.remove("output.zip")
+if (!dir.exists("data/output") || force) {
+    cat("Downloading occurrence data ======\n")
+    system("rm -r output output.zip")
+    download.file(
+        "https://obis-edna-results.s3.amazonaws.com/output.zip",
+        "output.zip"
+    )
+    unzip("output.zip", exdir = "data/")
+    file.remove("output.zip")
+} else {
+    cat("Occurrence data already downloaded ======\n")
+}
 
 # Prepare images database
-cat("Preparing image database ======\n")
-source("R/create_images_database.R")
+if (!dir.exists("images/gallery") || force) {
+    cat("Preparing image database ======\n")
+    source("R/create_images_database.R")
+} else {
+    cat("Image database done ======\n")
+}
